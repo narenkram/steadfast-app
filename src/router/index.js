5db0ab3b81a482477c01fef76ce83067552ec1e0
Narendra Ram <narenramm@gmail.com>
2024-05-21 17:12:46 +0530

    app home page and tradeview page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import CreateAccount from '../views/CreateAccount.vue'
 import AccountView from '../views/AccountView.vue'
+import TradeView from '../views/TradeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,11 +26,23 @@ const router = createRouter({
     {
       path: '/account',
       name: 'account',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AccountView.vue')
+      component: AccountView
+    },
+    {
+      path: '/trade-view',
+      name: 'trade-view',
+      component: TradeView
     }
+
+
+    // {
+    //   path: '/account',
+    //   name: 'account',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AccountView.vue')
+    // }
   ]
 })
 
