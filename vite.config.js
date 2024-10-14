5984d1fadf7bcf22ff73623ef3a9edf2096dc3b7
Narendra Ram <narenramm@gmail.com>
2024-10-14 12:09:36 +0530

    refactor: expose to localnetwork

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,8 @@ export default defineConfig({
     }
   },
   server: {
+    host: true,
+    port: 5173,
     proxy: {
       '/flattradeSymbols': 'http://localhost:3000/flattrade/',
       '/shoonyaSymbols': 'http://localhost:3000/shoonya/',
