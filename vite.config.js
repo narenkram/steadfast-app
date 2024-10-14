9bc485401040ae5478ddcf2ad82da0c816447a46
Narendra Ram <narenramm@gmail.com>
2024-10-14 12:57:08 +0530

    Revert "refactor: expose to localnetwork"
    This reverts commit 5984d1fadf7bcf22ff73623ef3a9edf2096dc3b7.

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,6 @@ export default defineConfig({
     }
   },
   server: {
-    host: true,
-    port: 5173,
     proxy: {
       '/flattradeSymbols': 'http://localhost:3000/flattrade/',
       '/shoonyaSymbols': 'http://localhost:3000/shoonya/',
