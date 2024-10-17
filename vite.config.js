263bef20ade1b4c27db059e54753169d8a159760
Narendra Ram <narenramm@gmail.com>
2024-10-18 02:29:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
   const isProduction = mode === 'production'
 
   return {
+    base: isProduction ? '/' : '/',
     plugins: [vue()],
     resolve: {
       alias: {
