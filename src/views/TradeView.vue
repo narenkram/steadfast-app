29d45795a042ea146caf1d4b0ed31faef8b663bf
Narendra Ram <narenramm@gmail.com>
2024-10-17 14:51:47 +0530

    refactor: move msg windows next to pnl

--- src/views/TradeView.vue
+++ src/views/TradeView.vue
@@ -47,65 +47,6 @@
     </div>
   </section>
 
-  <!-- Kill Switch MessageWindow -->
-  <section v-if="killSwitchActive" class="row my-2">
-    <div class="col-12">
-      <div class="bg-danger text-white p-3 rounded-3 shadow">
-        <div class="d-flex align-items-center justify-content-between">
-          <div>
-            <h5 class="mb-2">Kill switch activated</h5>
-            <p class="mb-0">
-              Trading has been blocked for the next 6 hours. Take a break to put your mind at ease.
-            </p>
-          </div>
-          <div class="text-center">
-            <span class="d-flex bg-white text-dark py-2 px-3 rounded-2 fs-6 fw-bold">
-              {{ killSwitchRemainingTime }}
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <!-- Trade Warning MessageWindow -->
-  <section v-if="riskReached && !killSwitchActive" class="row my-2">
-    <div class="col-12">
-      <div class="bg-warning text-dark p-3 rounded-3 shadow">
-        <div class="d-flex align-items-center justify-content-between">
-          <div>
-            <h5 class="mb-2">Trade Warning</h5>
-            <p class="mb-0">Risk Threshold Reached!</p>
-          </div>
-          <div class="text-center">
-            <span class="d-flex bg-white text-dark py-2 px-3 rounded-2 fs-6 fw-bold">
-              Make adjustments or close positions
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <!-- Trade Success MessageWindow -->
-  <section v-if="targetReached && !killSwitchActive" class="row my-2">
-    <div class="col-12">
-      <div class="bg-success text-white p-3 rounded-3 shadow">
-        <div class="d-flex align-items-center justify-content-between">
-          <div>
-            <h5 class="mb-2">Trade Success</h5>
-            <p class="mb-0">Target Reached!</p>
-          </div>
-          <div class="text-center">
-            <span class="d-flex bg-white text-dark py-2 px-3 rounded-2 fs-6 fw-bold">
-              Close positions to lock in profits
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
   <!-- Place Order Form -->
   <section class="row mt-2">
     <form @submit.prevent>
@@ -701,6 +642,65 @@
           {{ killSwitchButtonText }} <span v-if="killSwitchActive">{{ currentClockEmoji }}</span>
         </a>
       </span>
+    </div>
+  </section>
+
+  <!-- Kill Switch MessageWindow -->
+  <section v-if="killSwitchActive" class="row my-2">
+    <div class="col-12">
+      <div class="bg-danger text-white p-3 rounded-3 shadow">
+        <div class="d-flex align-items-center justify-content-between">
+          <div>
+            <h5 class="mb-2">Kill switch activated</h5>
+            <p class="mb-0">
+              Trading has been blocked for the next 6 hours. Take a break to put your mind at ease.
+            </p>
+          </div>
+          <div class="text-center">
+            <span class="d-flex bg-white text-dark py-2 px-3 rounded-2 fs-6 fw-bold">
+              {{ killSwitchRemainingTime }}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Trade Warning MessageWindow -->
+  <section v-if="riskReached && !killSwitchActive" class="row my-2">
+    <div class="col-12">
+      <div class="bg-warning text-dark p-3 rounded-3 shadow">
+        <div class="d-flex align-items-center justify-content-between">
+          <div>
+            <h5 class="mb-2">Trade Warning</h5>
+            <p class="mb-0">Risk Threshold Reached!</p>
+          </div>
+          <div class="text-center">
+            <span class="d-flex bg-white text-dark py-2 px-3 rounded-2 fs-6 fw-bold">
+              Make adjustments or close positions
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Trade Success MessageWindow -->
+  <section v-if="targetReached && !killSwitchActive" class="row my-2">
+    <div class="col-12">
+      <div class="bg-success text-white p-3 rounded-3 shadow">
+        <div class="d-flex align-items-center justify-content-between">
+          <div>
+            <h5 class="mb-2">Trade Success</h5>
+            <p class="mb-0">Target Reached!</p>
+          </div>
+          <div class="text-center">
+            <span class="d-flex bg-white text-dark py-2 px-3 rounded-2 fs-6 fw-bold">
+              Close positions to lock in profits
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 
