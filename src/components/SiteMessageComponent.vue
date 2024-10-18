9a101f77cc50ce607edc01df1f42a6c2f2a0b2e5
Narendra Ram <narenramm@gmail.com>
2024-10-18 12:09:17 +0530

    chore: update site message

--- src/components/SiteMessageComponent.vue
+++ src/components/SiteMessageComponent.vue
@@ -2,9 +2,14 @@
     <div class="seo-hidden">
         <h6 class="mb-0 bg-warning text-dark p-2">
             Regulatory Compliance Notice: If you identify any potential regulatory issues, please
-            <a href="mailto:email@narenkram.com" class="text-dark font-weight-bold">contact me at
+            <a href="mailto:email@narenkram.com" class="fw-bold text-dark">contact me at
                 email@narenkram.com</a>.
             I am committed to promptly addressing and resolving any concerns to ensure full compliance.
+        </h6>
+        <h6 class="mb-0 p-2">
+            🚧 Site Under Active Development: Some features may be experimental.
+            For any issues or feedback, please join our
+            <a href="https://t.me/steadfastapp" target="_blank" rel="noopener noreferrer">Telegram community</a>.
         </h6>
     </div>
 </template>
