17547994278dc3ab509f5531335bc322973d8769
Narendra Ram <narenramm@gmail.com>
2024-10-20 08:08:33 +0530

    new notification ui

--- src/components/AppNavigationComponent.vue
+++ src/components/AppNavigationComponent.vue
@@ -1,7 +1,6 @@
 <template>
-    <!-- steadfast-app/src/components/NavigationComponent.vue -->
-    <section class="Navigation">
-        <nav class="navbar navbar-expand-lg shadow-sm mt-0 mb-3">
+    <nav class="navbar navbar-expand-lg shadow-sm mt-0 mb-3">
+        <div class="container-fluid">
             <a class="navbar-brand p-0" href="#">
                 <img src="/steadfast_logo.png" class="Navigation__logo img-fluid" alt="Steadfast" />
             </a>
@@ -11,28 +10,32 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav justify-content-between w-100 mb-2 mb-lg-0">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item" v-for="route in routes" :key="route.path">
                         <RouterLink :to="route.path" class="nav-link"
                             :class="{ 'active-route': $route.path === route.path }">
                             {{ route.icon }} {{ route.name }}
                         </RouterLink>
                     </li>
-                    <!-- Toast Alert Component -->
-                    <li class="nav-item">
-                        <ToastAlertComponent :show="showToast" :message="toastMessage"
-                            :sound-enabled="notificationSound" :selected-sound="selectedSound"
-                            @update:show="updateToastVisibility" />
-                    </li>
                 </ul>
+                <!-- Always-on Notification Area -->
+                <div class="notification-area d-flex align-items-center">
+                    <div class="notification-icon me-2" :class="{ 'has-notification': showToast }">
+                        <i class="bi bi-bell-fill"></i>
+                    </div>
+                    <div class="notification-message" :class="{ 'show-message': showToast }">
+                        {{ toastMessage || 'No new notifications' }}
+                    </div>
+                    <button v-if="showToast" @click="updateToastVisibility(false)" class="btn btn-sm btn-close ms-2"
+                        aria-label="Close"></button>
+                </div>
             </div>
-        </nav>
-    </section>
+        </div>
+    </nav>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import ToastAlertComponent from '@/components/ToastAlertComponent.vue';
 import { useTradeView } from '@/composables/useTradingSystem';
 
 const {
