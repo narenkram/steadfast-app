10a2f2df746c3665ca6d9bcbba30b554c097b5ba
Narendra Ram <narenramm@gmail.com>
2024-10-26 12:20:53 +0530

    refactor: improve account ui

--- src/views/account/DashboardView.vue
+++ src/views/account/DashboardView.vue
@@ -1,28 +1,25 @@
 <template>
     <AppNavigationComponent />
-    <div class="container my-4">
-        <div class="row py-3">
-            <div class="col-md-6 mx-auto" v-if="user">
-                <h5 class="text-center mb-4">Account Information</h5>
-                <div class="mb-3">
-                    <label for="phoneNumber" class="form-label">Phone Number</label>
-                    <input type="text" id="phoneNumber" class="form-control" :value="user.phoneNumber" disabled>
-                </div>
-                <div class="mb-3">
-                    <label for="email" class="form-label">Email</label>
-                    <input type="email" id="email" class="form-control" :value="user.email" disabled>
-                </div>
-                <div class="mb-3">
-                    <label for="userId" class="form-label">User ID</label>
-                    <input type="text" id="userId" class="form-control" :value="user.uid" disabled>
-                </div>
-                <!-- Display other user details as needed -->
+    <div class="row py-3">
+        <div class="col-md-6 mx-auto rounded bg-color-2 py-3" v-if="user">
+            <h5 class="text-center mb-4">Account Information</h5>
+            <div class="mb-3">
+                <label for="phoneNumber" class="form-label">Phone Number</label>
+                <input type="text" id="phoneNumber" class="form-control" :value="user.phoneNumber" disabled>
             </div>
-            <div v-else class="col-12 text-center">
-                <p class="text-muted fw-bold m-0">Loading user details...</p>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" id="email" class="form-control" :value="user.email" disabled>
             </div>
+            <div class="mb-3">
+                <label for="userId" class="form-label">User ID</label>
+                <input type="text" id="userId" class="form-control" :value="user.uid" disabled>
+            </div>
+            <!-- Display other user details as needed -->
         </div>
-
+        <div v-else class="col-12 text-center">
+            <p class="text-muted fw-bold m-0">Loading user details...</p>
+        </div>
     </div>
 </template>
 
