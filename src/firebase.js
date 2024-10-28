d0ff1f44963e3a5fc20cd7267a575275403948e6
Narendra Ram <narenramm@gmail.com>
2024-10-28 17:05:32 +0530

    Revert "remove exposed api key"
    This reverts commit f9958696486b54d8fc129dcf7535ca95c30593bb.

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,20 @@
 import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
 import { getAuth } from 'firebase/auth'
-import { firebaseConfig } from './firebase.config'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyA3O3UfgQFs8pIzbbB_2SfH-bnkXP7B7RQ',
+  authDomain: 'steadfastapp-4d677.firebaseapp.com',
+  projectId: 'steadfastapp-4d677',
+  storageBucket: 'steadfastapp-4d677.appspot.com',
+  messagingSenderId: '901272268862',
+  appId: '1:901272268862:web:e0c1a7bc142dc88080a16f',
+  measurementId: 'G-K7SYZ5W0ER'
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
